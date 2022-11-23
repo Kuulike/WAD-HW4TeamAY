8bac3b39c9aa1e67c061053b6c2ff56f72f81f31
Priit Rooden <73076712+priitro@users.noreply.github.com>
2022-11-23 22:35:14 +0200

    Styling

--- hwvue/src/components/PostDisplayer.vue
+++ hwvue/src/components/PostDisplayer.vue
@@ -3,8 +3,9 @@
         <div class="post" v-for="post in postsList" :key="post.id">
             <div class="postinfo">
                 <div class="poster-logo">
-                    <p> {{post.timestamp}} </p>
                     <img src={{post.me}} alt="Profile Picture">
+                    <div class="post-date">{{post.timestamp}}</div>
+                
                 </div>
             </div>
         
@@ -59,6 +60,7 @@ export default{
     align-items: flex-start;
     background: rgb(243, 243, 225);
     height: auto;
+    width: 100%;
     border-radius: .5em;
     margin: 20px 0px 20px 1px;
     align-content: left;
@@ -73,6 +75,12 @@ export default{
     inline-size: 90%;
     overflow-wrap: break-word;
     padding: 3px 3px 3px 3px;
+}
+
+.post-text {
+    margin-right: 150px;
+    font-size: 18px;
+    text-align: left;
 }
 
 .post-date {
@@ -101,9 +109,46 @@ export default{
     margin: 10px 10px 10px 10px;
 }
 
-#content {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+
+
+
+
+
+.post-date {
+    align-content: right;
 }
+
+.post-picture img{
+    max-width: 350;
+    margin: 0px 0px 0px 20px;
+}
+
+.poster-logo img{
+    width: 35px;
+    height:35px;
+}
+
+.post-likes img{
+    width: 20px;
+    height:20px;
+}
+
+
+.post div+p{
+    font-size: 18px;
+    max-width: 350;
+    margin: 10px 10px 10px 10px;
+}
+
+@media (max-width: 770px) {
+
+    .post-picture{
+        align-self: center;
+    }
+
+    .post-picture img{
+        margin: 0;
+    }
+}
+
 </style>
