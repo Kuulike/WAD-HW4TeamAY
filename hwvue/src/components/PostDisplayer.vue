1817d7cb621066c133ef87b7e948f8979a7bf5b4
Tanel Tark <tanel.tark@ut.ee>
2022-11-23 17:53:24 +0200

    Initial start for creating posts

--- hwvue/src/components/PostDisplayer.vue
+++ hwvue/src/components/PostDisplayer.vue
@@ -1,15 +1,98 @@
 <template>
     <section>
         <h1>Siia teeme postitused!</h1>
+        <div class="post" v-for="post in postsList" :key="post.id">
+            <div class="postinfo">
+                <div class="poster-logo">
+                    <p> {{post.timestamp}} </p>
+                    <img src={{post.me}}>
+                </div>
+            </div>
+        
+            <div class="post-text"> {{post.body}} </div>
+            <div class="post-picture">
+                <img src={{post.postPicture}}>
+            </div>
+            
+            <div class="post-likes">
+                <img src={{post.like}}>
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
 export default{
     name: "PostDisplayer",
+    props: ["postsList"],
+    data: function() {return{}},
+    computed: {}
 }
 </script>
 
 <style>
+.postinfo {
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-flow: row wrap;
+}
 
+.postinfo div~p{
+    color: rgb(110, 33, 33);
+}
+
+.post {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    background: rgb(243, 243, 225);
+    height: auto;
+    border-radius: .5em;
+    margin: 20px 0px 20px 1px;
+    align-content: left;
+    align-items: stretch;    
+}
+
+.post div, .post p{
+    margin: 10px 10px 10px 10px;
+}
+
+.post-text {
+    inline-size: 90%;
+    overflow-wrap: break-word;
+    padding: 3px 3px 3px 3px;
+}
+
+.post-date {
+    align-content: right;
+}
+
+.post-picture img{
+    max-width: 500px;
+    margin: 0px 0px 0px 0px;
+}
+
+.poster-logo img{
+    width: 35px;
+    height:35px;
+}
+
+.post-likes img{
+    width: 20px;
+    height:20px;
+}
+
+
+.post div+p{
+    font-size: 18px;
+    max-width: 350;
+    margin: 10px 10px 10px 10px;
+}
+
+#content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
 </style>
--- hwvue/src/views/HomeView.vue
+++ hwvue/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
-  <PostDisplayer></PostDisplayer>
+  <PostDisplayer :postsList="postsList"></PostDisplayer>
 </template>
 
 <script>
@@ -14,6 +14,33 @@ export default {
   components: {
     PostDisplayer,
 
+  },
+
+  data: function() {
+    return {
+      postsList:
+      [
+        {
+          id:1,
+          timestamp: "Sept 30, 2022",    
+          title:"ajalugu",
+          me:"https://owncloud.ut.ee/owncloud/apps/files_sharing/publicpreview/K2yERnxKKAZJs6W?x=1905&y=505&a=true&file=me.png&scalingup=0",
+          body: "Tartu keskse väljaku, Suurturu arhitektuuri tähtsamateks ehitisteks saidki uus raekoda väljaku laiemas ja perspektiivi lõpetav Kivisild väljaku kitsamas osas.",
+          postPicture:"f",
+          like:"../assets/likelogo.png"
+      
+      },
+      {
+          id:2,
+          timestamp: "Sept 29, 2022",
+          title:"tartu logo",
+          me:"https://owncloud.ut.ee/owncloud/apps/files_sharing/publicpreview/K2yERnxKKAZJs6W?x=1905&y=505&a=true&file=me.png&scalingup=0",    
+          body: "Tartu uus logo",
+          postPicture:"https://owncloud.ut.ee/owncloud/s/Q9BMYEEapfosC74/preview",
+          like:"../assets/likelogo.png" 
+      }
+      ]
+    }
   }
 }
 </script>
