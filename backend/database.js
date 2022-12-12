1b378e3baa276fb28a8a6f1f9710d965177b9bb5
Tanel Tark <tanel.tark@ut.ee>
2022-12-12 17:45:21 +0200

    installed backend dependencies fixed frontend password validation

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "[add your password here]",
+    password: "Punnsilm",
     database: "testWAD",
     host: "localhost",
     port: "5432"
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,33 @@
 // server.js
 const express = require('express');
 const pool = require('./database');
-const cors = require('cors')
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+// We need to include "credentials: true" to allow cookies to be represented  
+// Also "credentials: 'include'" need to be added in Fetch API in the Vue.js App
 
-// The express.json() function is a built-in middleware function in Express. 
-// It parses incoming requests with JSON payloads and is based on body-parser.
-app.use(express.json());
+app.use(express.json());  // Parses incoming requests with JSON payloads and is based on body-parser.
+app.use(cookieParser());  // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
 
+const secret = "gdgdhdbcb770785rgdzqws"; // use a stronger secret
+const maxAge = 60 * 60; //unlike cookies, the expiresIn in jwt token is calculated by seconds not milliseconds
+
+const generateJWT = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: maxAge })
+        //jwt.sign(payload, secret, [options, callback]), and it returns the JWT as string
+}
 ``````````````
-    //  Handling HTTP requests code will go here  
+    // Code will go here
+
+
 
 ``````````````
 app.listen(port, () => {
