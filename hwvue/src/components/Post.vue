a460b4c619d534e3cade1ac1946586fb0694f0d7
Tanel Tark <tanel.tark@ut.ee>
2022-12-15 01:12:17 +0200

    fixed dates on posts

--- hwvue/src/components/Post.vue
+++ hwvue/src/components/Post.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="post">
     <div class="postinfo">
-        <div class="post-date">{{post.timestamp}}</div>
+        <div class="post-date">{{post.timestamp.slice(0, 10)}}</div>
     </div>
     <div class="post-text"> {{post.body}} </div>
 </div>
